589cec05efd6c0277a828be05c5ebd22c079a7f9
taitamfc <taitam8590@gmail.com>
2022-07-18 08:44:00 +0700

    Migration_Seeder

---
+++ database/migrations/2022_07_18_013553_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);//=> name varchar(255)
+            $table->unsignedBigInteger('type_id');//=> type_id bigint(20)
+            $table->foreign('type_id')->references('id')->on('types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
